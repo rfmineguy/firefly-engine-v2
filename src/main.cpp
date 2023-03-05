3d9db120e4fec84b98cae1c6f6f4a3f62033b1a9
rfmineguy <rfmineguy@gmail.com>
2023-03-04 19:00:58 -0800

    Initial commit of ff-engine2

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+#include <iostream>
+
+void error_callback(int error, const char* description) {
+  std::cerr << "GLFWError: " << description << std::endl;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main() {
+  if (!glfwInit()) {
+    glfwTerminate();
+  }
+  glfwSetErrorCallback(error_callback);
+  
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  GLFWwindow* window = glfwCreateWindow(600, 600, "Window", NULL, NULL);
+  if (!window) {
+    std::cerr << "FFError: Failed to create glfw window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "FFError: Failed to initialize glad" << std::endl;
+    glfwTerminate();
+    return -2;
+  }
+  glViewport(0, 0, 600, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }  
+  glfwDestroyWindow(window);
+  glfwTerminate();
+}
